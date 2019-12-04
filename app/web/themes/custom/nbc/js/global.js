11e4a46ceae66e38cb5073353459c683bca8c169
Cesar <cesarg@ci.garden-grove.ca.us>
2019-12-04 05:35:55 -0800

    Initial commit

---
+++ app/web/themes/custom/nbc/js/global.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.nbc = {
+    attach: function (context, settings) {
+
+  
+    }
+  };
+
+})(jQuery, Drupal);
---
+++ app/web/themes/custom/nbc/js/nbcCopyToClipboard.js
@@ -0,0 +1,24 @@
+function selectElementContents(el) {
+	var body = document.body, range, sel;
+	if (document.createRange && window.getSelection) {
+		range = document.createRange();
+		sel = window.getSelection();
+		sel.removeAllRanges();
+		try {
+			range.selectNodeContents(el);
+			sel.addRange(range);
+		} catch (e) {
+			range.selectNode(el);
+			sel.addRange(range);
+		}
+	} else if (body.createTextRange) {
+		range = body.createTextRange();
+		range.moveToElementText(el);
+		range.select();
+  }
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Table copied to clipboard.");
+}
